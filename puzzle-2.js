74a1cb323f1db5557b4466b8431817623aecdd61
fredgoncalves <fleitegoncalves@cca.edu>
2016-01-27 00:41:07 -0800

    Create puzzle-2.js

---
+++ puzzle-2.js
@@ -0,0 +1,23 @@
+var counter;
+for(counter=0;counter<6;counter++){
+
+down();
+  
+}
+
+for (counter=0;counter<3;counter++){
+
+right();
+
+}
+
+up();
+up();
+right();
+right();
+
+for (counter=0;counter<3;counter++){
+
+down();
+
+}
