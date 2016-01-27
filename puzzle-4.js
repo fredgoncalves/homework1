86f77ef3d4b19a8507c0314509cb5984382d075d
fredgoncalves <fleitegoncalves@cca.edu>
2016-01-27 01:12:24 -0800

    Create puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,28 @@
+var color;
+var counter;
+
+for(counter=0;counter<4;counter++){
+down();
+}
+
+right();
+
+color = getColor();
+
+down();
+
+setColor(color);
+
+right();
+right();
+up();
+right();
+
+color = getColor();
+
+down();
+
+setColor(color);
+
+right();
+right();
