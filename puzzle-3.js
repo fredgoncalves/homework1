c716686b31011b6ea13af4eee4fc30ff13c8b82b
fredgoncalves <fleitegoncalves@cca.edu>
2016-01-27 01:07:31 -0800

    Create puzzle-3.js

---
+++ puzzle-3.js
@@ -0,0 +1,17 @@
+var color;
+
+down();
+down();
+color = getColor();
+
+var counter;
+for(counter=0;counter<3;counter++){
+down();
+}
+
+right();
+right();
+setColor(color);
+right();
+right();
+up();
