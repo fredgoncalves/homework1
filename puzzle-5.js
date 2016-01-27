e1ed6215ed9c1cdcc873df4409642ae53902fc76
fredgoncalves <fleitegoncalves@cca.edu>
2016-01-27 01:17:45 -0800

    Create puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,22 @@
+var counter;
+
+
+right();
+right();
+
+for (counter=0;counter<3;counter++){
+if (getColor() == "blue" ){
+down();
+right();
+right();
+
+}
+
+else{
+up();
+right();
+right();
+  
+}
+}
+right();
